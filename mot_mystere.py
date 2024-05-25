c34e157897eb2cf5b7de3cebb4fa7d6634c1b84a
arthurklr_ <arthur.k2104@gmail.com>
2024-05-25 19:50:00 +0200

    Update mot_mystere.py

--- mot_mystere.py
+++ mot_mystere.py
@@ -1,10 +1,16 @@
-mot_mystere = 'python'
+import random
 
+# Liste des mots mystères
+liste_mots = ['python', 'ordinateur', 'programmation', 'developpeur', 'algorithme']
+
+# Choisir un mot mystère aléatoirement
+mot_mystere = random.choice(liste_mots)
+
+# Initialiser les variables
 mot_public = '_' * len(mot_mystere)
-
 vie = 7
 
-
+# Boucle principale du jeu
 while vie > 0 and mot_mystere != mot_public:
     lettre = input("Donnez une lettre : ")
     if lettre in mot_mystere:
@@ -13,10 +19,11 @@ while vie > 0 and mot_mystere != mot_public:
                 mot_public = mot_public[:i] + lettre + mot_public[i + 1 :]
     else: 
         vie -= 1
+    
     if mot_public == mot_mystere:
-        print("Vous avez gagnez, le mot était", mot_mystere)
-    elif vie ==0:
+        print("Vous avez gagné, le mot était", mot_mystere)
+    elif vie == 0:
         print("Vous avez perdu")
     else:
-        print("Vous avez", vie)
+        print("Vous avez", vie, "vies restantes.")
         print("Le mot est :", mot_public)
