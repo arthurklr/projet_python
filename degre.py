2d67374e51fb37de04d0da5edb93e24d89f9434a
arthurklr_ <arthur.k2104@gmail.com>
2024-06-01 16:55:32 +0200

    Update degre.py

--- degre.py
+++ degre.py
@@ -1,7 +1,10 @@
 # Programme qui convertit des °C en °F
 
+# Demande à l'utilisateur d'entrer une température en degrés Celsius
 degre_celcius = float(input ("Entrez des degrés Celsius : "))
 
-degre_fahrenheit = degre_celcius * 9/5 +32 
+# Convertit la température de Celsius en Fahrenheit en utilisant la formule de conversion
+degre_fahrenheit = degre_celcius * 9/5 + 32 
 
+# Affiche le résultat de la conversion
 print(degre_celcius,"°C correspond à", degre_fahrenheit,"°F")
