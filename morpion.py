66a1d10428fb4ebf739dc50a00396f5858fbb7dc
arthurklr_ <arthur.k2104@gmail.com>
2024-06-01 16:55:24 +0200

    Update morpion.py

--- morpion.py
+++ morpion.py
@@ -1,10 +1,10 @@
 def afficher_grille(grille):
+    # Affiche la grille de jeu
     print("     0)  1)  2)")
     print("   -------------")
     print("0)", end='')
     for i in range(3):
         print(" | "+str(grille[i]), end='')
-        
     print(" |")
     print("   -------------")
     print("1)", end='')
@@ -19,16 +19,19 @@ def afficher_grille(grille):
     print("   -------------")
 
 def tour(grille, joueur):
+    # Effectue le tour du joueur en demandant une colonne et une ligne où jouer
     print("C'est le tour du joueur " +str(joueur))
     colonne=input("Entrez le numéro de la colonne : ")
     ligne=input("Entrez le numéro de la ligne : ")
     print("Vous avez joué la case ("+colonne+", "+ligne+")") 
+    # Vérifie si la case est déjà occupée
     while grille[int(colonne)+int(ligne)*3]!= " ":
         afficher_grille(grille)
         print("Cette case est déjà jouée ! Saisissez une autre case svp !")
         colonne=input("Entrez le numéro de la colonne : ")
         ligne=input("Entrez le numéro de la ligne : ")
         print("Vous avez joué la case ("+colonne+", "+ligne+")")
+    # Place le symbole du joueur dans la case choisie
     if joueur ==1:
         grille[int(colonne)+int(ligne)*3]="X"
     if joueur ==2:
@@ -36,6 +39,7 @@ def tour(grille, joueur):
     afficher_grille(grille)
     
 def est_gagnant(grille):
+    # Vérifie s'il y a un gagnant sur la grille
     if (grille[0]==grille[1]) and (grille[0]==grille[2]) and (grille[0]!=" "):
         return 1
     if (grille[3]==grille[4]) and (grille[3]==grille[5]) and (grille[3]!=" "):
@@ -54,18 +58,23 @@ def est_gagnant(grille):
         return 1
     
 def est_match_nul(grille):
+    # Vérifie si la grille est pleine, indiquant un match nul
     for i in range(9):
         if grille[i]==" ":
             return 0
     return 1
 
+# Initialisation du jeu
 joueur=1
 print("Le joueur 1 possède les X. Le joueur 2 possède les O")
 grille=[" "," "," "," "," "," "," "," "," "]
 afficher_grille(grille)
 gagne=0
+
+# Boucle de jeu
 while gagne==0:
     tour(grille, joueur)
+    # Vérifie s'il y a un gagnant ou un match nul
     if est_gagnant(grille):
         print("Le joueur "+str(joueur)+" remporte la partie")
         gagne = 1
@@ -73,6 +82,7 @@ while gagne==0:
         if est_match_nul(grille):
             print("Plus de place ! Match nul !")
             gagne = 1
+    # Passe au joueur suivant
     if joueur ==1:
         joueur =2
     else:
