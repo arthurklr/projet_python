57c134d5e67ece7853be699d7e5278bec9e72b5e
arthurklr_ <arthur.k2104@gmail.com>
2024-06-01 16:55:23 +0200

    Update liste.py

--- liste.py
+++ liste.py
@@ -1,13 +1,14 @@
 # Programme qui affiche une liste de 0 à n-1 (à définir)
 
 def simple_range(n):
-    l = []
-    i = 0
+    # Définition d'une fonction simple_range qui prend un argument n
+    l = []  # Initialisation d'une liste vide
+    i = 0   # Initialisation d'un compteur à 0
     
-    while i < n:
-        l.append(i)
-        i += 1
-        
-    return l
+    while i < n:  # Boucle tant que le compteur est inférieur à n
+        l.append(i)  # Ajout de la valeur actuelle du compteur à la liste
+        i += 1  # Incrémentation du compteur
+    
+    return l  # Renvoie la liste complète
 
-print(simple_range(10))
+print(simple_range(10))  # Appel de la fonction simple_range avec n=10 et affichage du résultat
