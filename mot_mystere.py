f791bc41932c51673e59b3043689dae35fd785ed
arthurklr_ <arthur.k2104@gmail.com>
2024-06-01 16:55:26 +0200

    Update mot_mystere.py

--- mot_mystere.py
+++ mot_mystere.py
@@ -9,27 +9,36 @@ liste_mots = ['marissa', 'corentin', 'alain', 'matheo']
 mot_mystere = random.choice(liste_mots)
 
 # Initialiser les variables
-mot_public = '_' * len(mot_mystere)
-vie = len(mot_mystere)
-lettre_utilisees = []
+mot_public = '_' * len(mot_mystere)  # Initialiser le mot public avec des tirets bas pour chaque lettre du mot mystère
+vie = len(mot_mystere)  # Nombre de vies initialisé à la longueur du mot mystère
+lettre_utilisees = []  # Liste pour stocker les lettres déjà utilisées
 
 # Boucle principale du jeu
 while vie > 0 and mot_mystere != mot_public:
-    lettre = input("Donnez une lettre : ")
-    if len(lettre) !=1:
+    lettre = input("Donnez une lettre : ")  # Demander au joueur de deviner une lettre
+
+    # Vérifier si la saisie est une seule lettre
+    if len(lettre) != 1:
         print("Veuillez entrer qu'une seule lettre.")
         continue
+
+    # Vérifier si la lettre a déjà été utilisée
     if lettre in lettre_utilisees:
         print("Vous avez déjà utilisé cette lettre")
         continue
-    lettre_utilisees.append(lettre)
+
+    lettre_utilisees.append(lettre)  # Ajouter la lettre à la liste des lettres utilisées
+
+    # Vérifier si la lettre devinée est dans le mot mystère
     if lettre in mot_mystere:
+        # Mettre à jour le mot public pour révéler les occurrences de la lettre devinée
         for i in range(len(mot_mystere)):
             if mot_mystere[i] == lettre:
                 mot_public = mot_public[:i] + lettre + mot_public[i + 1 :]
-    else: 
-        vie -= 1
+    else:
+        vie -= 1  # Décrémenter le nombre de vies si la lettre devinée n'est pas dans le mot mystère
     
+    # Afficher le résultat du tour
     if mot_public == mot_mystere:
         print("Vous avez gagné, le mot était", mot_mystere)
     elif vie == 0:
